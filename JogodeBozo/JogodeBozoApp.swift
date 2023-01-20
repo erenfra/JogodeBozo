32826300b1ffa63ec99ff38ec556baf6af037ae7
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-20 15:36:31 -0600

    Initial Commit

---
+++ JogodeBozo/JogodeBozoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogodeBozoApp.swift
+//  JogodeBozo
+//
+//  Created by Renato Oliveira Fraga on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct JogodeBozoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
